bf99bb749093512f0329150a3f463dd9d66f1cb2
jayditya <jayditya.sawai@gmail.com>
2025-07-21 16:28:31 +0530

    fix: redirect root URL to /support/

--- django_app/insightdesk/urls.py
+++ django_app/insightdesk/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
 
 urlpatterns = [
+    path('', lambda request: redirect('support/', permanent=False)),
     path('admin/', admin.site.urls),
     path('support/', include('support.urls')),
-    path('dashboard/', include('dashboard.urls')), 
-
+    path('dashboard/', include('dashboard.urls')),
 ]
